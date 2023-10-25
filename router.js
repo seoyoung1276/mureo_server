1b053ae7ff57d863dcbd6b56d2024737d6c8410d
seoyoung1276 <seoyoung1276@gmail.com>
2023-10-25 09:32:41 +0900

    데이터베이스 연결

--- router.js
+++ router.js
@@ -1,5 +1,12 @@
+const db = require('../db/config')
+
 //클라이언트에서 값을 받아오기 위한 코드
-Router.post('/register', (req, res,next => {
+Router.post('/register', (req, res,next) => {
+    const param = [req.body.id, req. body.pw, req.body.name]
+
+    db.qurey('INSERT INTO user(`user_name`,`user_id`,`password`,`email`) VALUES (?,?,?)', param, (err, row) =>{
+        if(err) console.log(err)
+    })
     console.log(req.body)
     res.end() 
-}))
+})
